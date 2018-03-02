c398aedce8645d16445ccdd1f18d8cc61ab5e1af
moritz-a <36733619+moritz-a@users.noreply.github.com>
2018-03-02 15:00:25 +0100

    servlet

--- src/main/java/ch/fhnw/digibp/ProcessApplicationServlet.java
+++ src/main/java/ch/fhnw/digibp/ProcessApplicationServlet.java
@@ -1,13 +1,16 @@
 package ch.fhnw.digibp;
 
-import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+//import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import org.camunda.bpm.application.ProcessApplication;
+
 @SpringBootApplication
-@EnableProcessApplication
+//@EnableProcessApplication
+@ProcessApplication("Incident")
 public class ProcessApplicationServlet extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
