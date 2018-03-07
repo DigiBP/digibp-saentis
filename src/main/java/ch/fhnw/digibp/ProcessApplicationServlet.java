1467d0bf0854828992f82830648e334ed56c689c
moritz-a <36733619+moritz-a@users.noreply.github.com>
2018-03-07 10:10:44 +0100

    add blocker task

--- src/main/java/ch/fhnw/digibp/ProcessApplicationServlet.java
+++ src/main/java/ch/fhnw/digibp/ProcessApplicationServlet.java
@@ -20,6 +20,8 @@ public class ProcessApplicationServlet extends SpringBootServletInitializer {
 			//	  .createProcessEngineConfigurationFromResourceDefault()
 				//  .setHistory(ProcessEngineConfiguration.HISTORY_FULL)
 				  //.buildProcessEngine();
+		
+		
 	}
 
 	@Override
