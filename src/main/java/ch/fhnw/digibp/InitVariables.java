967d8593c5e931d56776ea1ca0ef01e8202127b3
moritz-a <36733619+moritz-a@users.noreply.github.com>
2018-03-27 09:18:13 +0200

    Variable Init

---
+++ src/main/java/ch/fhnw/digibp/InitVariables.java
@@ -0,0 +1,72 @@
+package ch.fhnw.digibp;
+
+import java.util.logging.Logger;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
+
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+//do some logging for reporting
+
+//maybe we can work with that: https://docs.camunda.org/manual/7.8/user-guide/process-engine/history/#user-operation-log 
+public class InitVariables implements JavaDelegate {
+
+  private final static Logger LOGGER = Logger.getLogger("InitVars started");
+  public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+  
+  private DelegateExecution execution;
+  
+  private void setIfNull(String varName, String value) {
+	  
+	    LOGGER.info("Check variable content of '" +  varName + "' defined as '" + execution.getVariable(varName) + "'");
+		  
+		  Object var = execution.getVariable(varName);
+		  if (var == null)
+		  {
+			  execution.setVariable(varName, value);
+			    LOGGER.info("set to '" + varName + "' to '" + value + "'");
+		  }
+}
+  
+  private void setVar(String varName, String value) {
+	  
+	    execution.setVariable(varName, value);
+		LOGGER.info("set to '" + varName + "' to '" + value + "'");
+}
+
+  public static String now() {
+	  Calendar cal = Calendar.getInstance();
+	  SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+	  return sdf.format(cal.getTime());
+	  }
+
+  public void execute(DelegateExecution execution) throws Exception {
+
+	  this.execution = execution;
+	  
+	  setIfNull("impact", "3_MEDIUM");
+	  setIfNull("urgency", "3_MEDIUM");
+	  setIfNull("priority", "3_MEDIUM");
+	  setIfNull("affectedApplication", "undefined");
+	  setIfNull("summary", "");
+	  setIfNull("description", "");
+	  setIfNull("systemID", "");
+	  setIfNull("stepByStep", "");
+	  setIfNull("customerName", "");
+	  setIfNull("customerMail", "");
+	  setIfNull("customerPhone", "");
+	  
+	  setVar("ticketOpenDate", now());
+	  setVar("ticketStatus", "NEW");
+	  setVar("ticketOrigin", "MANUAL");
+	  
+	  
+	  setIfNull("isReallyIncident", "");
+	  setIfNull("incidentSupported", "");
+
+	  
+  }
+
+}
