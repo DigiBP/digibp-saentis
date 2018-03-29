31b2195e5af3789ce7d7e1193be2330979a7aed3
moritz-a <36733619+moritz-a@users.noreply.github.com>
2018-03-29 08:36:22 +0200

    reformat

--- src/main/java/ch/fhnw/digibp/InitVariables.java
+++ src/main/java/ch/fhnw/digibp/InitVariables.java
@@ -4,8 +4,6 @@ import java.util.logging.Logger;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
 
-
-
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 //do some logging for reporting
@@ -13,60 +11,57 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 //maybe we can work with that: https://docs.camunda.org/manual/7.8/user-guide/process-engine/history/#user-operation-log 
 public class InitVariables implements JavaDelegate {
 
-  private final static Logger LOGGER = Logger.getLogger("InitVars started");
-  public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
-  
-  private DelegateExecution execution;
-  
-  private void setIfNull(String varName, String value) {
-	  
-	    LOGGER.info("Check variable content of '" +  varName + "' defined as '" + execution.getVariable(varName) + "'");
-		  
-		  Object var = execution.getVariable(varName);
-		  if (var == null)
-		  {
-			  execution.setVariable(varName, value);
-			    LOGGER.info("set to '" + varName + "' to '" + value + "'");
-		  }
-}
-  
-  private void setVar(String varName, String value) {
-	  
-	    execution.setVariable(varName, value);
+	private final static Logger LOGGER = Logger.getLogger("InitVars started");
+	public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+
+	private DelegateExecution execution;
+
+	private void setIfNull(String varName, String value) {
+
+		LOGGER.info("Check variable content of '" + varName + "' defined as '" + execution.getVariable(varName) + "'");
+
+		Object var = execution.getVariable(varName);
+		if (var == null) {
+			execution.setVariable(varName, value);
+			LOGGER.info("set to '" + varName + "' to '" + value + "'");
+		}
+	}
+
+	private void setVar(String varName, String value) {
+
+		execution.setVariable(varName, value);
 		LOGGER.info("set to '" + varName + "' to '" + value + "'");
-}
+	}
 
-  public static String now() {
-	  Calendar cal = Calendar.getInstance();
-	  SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
-	  return sdf.format(cal.getTime());
-	  }
+	public static String now() {
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+		return sdf.format(cal.getTime());
+	}
 
-  public void execute(DelegateExecution execution) throws Exception {
+	public void execute(DelegateExecution execution) throws Exception {
 
-	  this.execution = execution;
-	  
-	  setIfNull("impact", "3_MEDIUM");
-	  setIfNull("urgency", "3_MEDIUM");
-	  setIfNull("priority", "3_MEDIUM");
-	  setIfNull("affectedApplication", "undefined");
-	  setIfNull("summary", "");
-	  setIfNull("description", "");
-	  setIfNull("systemID", "");
-	  setIfNull("stepByStep", "");
-	  setIfNull("customerName", "");
-	  setIfNull("customerMail", "");
-	  setIfNull("customerPhone", "");
-	  
-	  setVar("ticketOpenDate", now());
-	  setVar("ticketStatus", "NEW");
-	  setVar("ticketOrigin", "MANUAL");
-	  
-	  
-	  setIfNull("isReallyIncident", "");
-	  setIfNull("incidentSupported", "");
+		this.execution = execution;
 
-	  
-  }
+		setIfNull("impact", "3");
+		setIfNull("urgency", "3");
+		setIfNull("priority", "3");
+		setIfNull("affectedApplication", "undefined");
+		setIfNull("summary", "");
+		setIfNull("description", "");
+		setIfNull("systemID", "");
+		setIfNull("stepByStep", "");
+		setIfNull("customerName", "");
+		setIfNull("customerMail", "");
+		setIfNull("customerPhone", "");
+
+		setVar("ticketOpenDate", now());
+		setVar("ticketStatus", "NEW");
+		setVar("ticketOrigin", "MANUAL");
+
+		setIfNull("isReallyIncident", "");
+		setIfNull("incidentSupported", "");
+
+	}
 
 }
--- src/main/java/ch/fhnw/digibp/SendConfirmationEmail.java
+++ src/main/java/ch/fhnw/digibp/SendConfirmationEmail.java
@@ -7,67 +7,54 @@ import org.camunda.bpm.engine.impl.util.json.JSONObject;
 import org.camunda.connect.Connectors;
 import org.camunda.connect.httpclient.HttpConnector;
 import org.camunda.connect.httpclient.HttpRequest;
+
 //do some logging for reporting
 //maybe we can work with that: https://docs.camunda.org/manual/7.8/user-guide/process-engine/history/#user-operation-log 
 public class SendConfirmationEmail implements JavaDelegate {
 
-  private final static Logger LOGGER = Logger.getLogger("EMAIL");
+	private final static Logger LOGGER = Logger.getLogger("EMAIL");
 
-  public void execute(DelegateExecution execution) throws Exception {
-    LOGGER.info("I will now send an e-mail to '" + execution.getVariable("emailAddress") + "' concerning " + execution.getVariable("AffectedApplication"));
-  
-    HttpConnector http = Connectors.getConnector(HttpConnector.ID);
-    HttpRequest request = http.createRequest();
-    request.setRequestParameter("method", "POST");
-    request.setRequestParameter("url", "https://hooks.zapier.com/hooks/catch/3017214/katznf/");
+	public void execute(DelegateExecution execution) throws Exception {
+		LOGGER.info("I will now send an e-mail to '" + execution.getVariable("emailAddress") + "' concerning "
+				+ execution.getVariable("AffectedApplication"));
 
-    request.header("Content-Type", "application/json");
-    
-    
-    
-    String email = (String)execution.getVariable("customerMail");
+		HttpConnector http = Connectors.getConnector(HttpConnector.ID);
+		HttpRequest request = http.createRequest();
+		request.setRequestParameter("method", "POST");
+		request.setRequestParameter("url", "https://hooks.zapier.com/hooks/catch/3017214/katznf/");
 
-    String msg = "Dear " + execution.getVariable("customerName") + "\n." +
-    "This is an automated message to notify you on the change of your issue.\n\n" +
-    "\nSummary: " + execution.getVariable("summary")  +
-    "\nDescription: " + execution.getVariable("description")  +
-    "\nTicket Open Date: " + execution.getVariable("ticketOpenDate")  +
-    "\nTicket Status: " + execution.getVariable("ticketStatus")  +
-    "\nTicket Origin: " + execution.getVariable("ticketOrigin")  +    
-    "\nImpact: " + execution.getVariable("impact")  +
-    "\nUrgency: " + execution.getVariable("urgency") +
-    "\nPriority: " + execution.getVariable("priority")  +
-    "\nAffected Application: " + execution.getVariable("affectedApplication")  +
+		request.header("Content-Type", "application/json");
 
-    "\nSystem ID: " + execution.getVariable("systemID")  +
-    "\nStep by Step Instruction: " + execution.getVariable("stepByStep")  +
-    "\nCustomer Name: " + execution.getVariable("customerName")  +
-    "\nCustomer Mail: " + execution.getVariable("customerMail")  +
-    "\nCustomer Phone: " + execution.getVariable("customerPhone")  +
+		String email = (String) execution.getVariable("customerMail");
 
-    "\nReal Incident: " + execution.getVariable("isReallyIncident")  +
-    "\nSupported: " + execution.getVariable("incidentSupported")  +
+		String msg = "Dear " + execution.getVariable("customerName") + "\n."
+				+ "This is an automated message to notify you on the change of your issue.\n\n" + "\nSummary: "
+				+ execution.getVariable("summary") + "\nDescription: " + execution.getVariable("description")
+				+ "\nTicket Open Date: " + execution.getVariable("ticketOpenDate") + "\nTicket Status: "
+				+ execution.getVariable("ticketStatus") + "\nTicket Origin: " + execution.getVariable("ticketOrigin")
+				+ "\nImpact: " + execution.getVariable("impact") + "\nUrgency: " + execution.getVariable("urgency")
+				+ "\nPriority: " + execution.getVariable("priority") + "\nAffected Application: "
+				+ execution.getVariable("affectedApplication") +
 
-    
-    
-    "\n We will try to get back to you shorty.\n\nBest Regards, Säntis Group";
-    
-    		
-    LOGGER.info("Message: " + msg);
+				"\nSystem ID: " + execution.getVariable("systemID") + "\nStep by Step Instruction: "
+				+ execution.getVariable("stepByStep") + "\nCustomer Name: " + execution.getVariable("customerName")
+				+ "\nCustomer Mail: " + execution.getVariable("customerMail") + "\nCustomer Phone: "
+				+ execution.getVariable("customerPhone") +
 
-    
-    String json = new JSONObject()
-    		  .put("email", email)
-    		  .put("msg", msg)
-    		  .put("title", "Incident Notification")
-    		  .toString();
-    
-    
-    
-    request.setRequestParameter("payload", json);
-    
-    request.execute();
-    
-  }
+				"\nReal Incident: " + execution.getVariable("isReallyIncident") + "\nSupported: "
+				+ execution.getVariable("incidentSupported") +
+
+				"\n We will try to get back to you shorty.\n\nBest Regards, Säntis Group";
+
+		LOGGER.info("Message: " + msg);
+
+		String json = new JSONObject().put("email", email).put("msg", msg).put("title", "Incident Notification")
+				.toString();
+
+		request.setRequestParameter("payload", json);
+
+		request.execute();
+
+	}
 
 }
